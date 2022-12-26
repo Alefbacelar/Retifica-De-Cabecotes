10e6421eafba7722a1c0aa41e15bd7c16703533d
ALEF BACELAR <74435383+Alefbacelar@users.noreply.github.com>
2022-12-26 17:15:38 -0300

    Iniciando Projeto

--- retifica/src/app/app-routing.module.ts
+++ retifica/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', redirectTo: 'home', pathMatch:'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
